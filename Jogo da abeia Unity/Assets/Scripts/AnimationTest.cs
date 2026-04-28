ed7008169476b87f0d51ab08033ed63390ab4d6d
Zarinbabau <miguelbpgomes@gmail.com>
2026-04-28 19:15:55 -0300

    Script de teste
    Script de teste não finalizado da animação da abelha

--- Jogo da abeia Unity/Assets/Scripts/AnimationTest.cs
+++ Jogo da abeia Unity/Assets/Scripts/AnimationTest.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class AnimationTest : MonoBehaviour
 {
+
+
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,21 @@ public class AnimationTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
