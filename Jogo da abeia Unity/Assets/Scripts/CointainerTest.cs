0f1162aea010f6078f7b3146fb3981665d7f4605
Inkkaaa <henrygbastos@gmail.com>
2026-05-12 16:58:04 -0300

    esqueci

--- Jogo da abeia Unity/Assets/Scripts/CointainerTest.cs
+++ Jogo da abeia Unity/Assets/Scripts/CointainerTest.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class CointainerTest : MonoBehaviour
 {
+
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,21 @@ public class CointainerTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+//sistema de selecionar pote
+// 0 - 0 - 0 - 0 - 0(vazio) 
+
+//if (aperta x){
+
+//reseta}
+
+
+if aperta enter pote a, checa o pote e permite selecionar outro pote before
+if aperta enter pote a e pote b selcionados e determianda condicao:
+if pote a > B subtraia a-B
+
+else pote b<a 
+
+
     }
 }
